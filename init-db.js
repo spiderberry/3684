ab4994f27bcd6e956f4b2db826eb62b5c5b25806
spiderberry <159087579+spiderberry@users.noreply.github.com>
2026-06-18 12:34:29 -0400

    rename init-db & refactor
    create seed script for db

--- init-db.js
+++ init-db.js
@@ -18,11 +18,23 @@ db.prepare(`
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
+    gender TEXT NOT NULL,
     country_id INTEGER NOT NULL,
-    year INTEGER NOT NULL,
-    sb_100m REAL NOT NULL,
-    sb_200m REAL NOT NULL,
     FOREIGN KEY (country_id) REFERENCES countries(id)
+  )
+`).run();
+
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS athlete_seasons (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    athlete_id INTEGER NOT NULL,
+    year INTEGER NOT NULL,
+    sb_60m REAL,
+    sb_100m REAL,
+    sb_200m REAL,
+    sb_400m REAL,
+    FOREIGN KEY (athlete_id) REFERENCES athletes(id),
+    CONSTRAINT chk_at_least_one CHECK (sb_60m IS NOT NULL OR sb_100m IS NOT NULL OR sb_200m IS NOT NULL OR sb_400m IS NOT NULL)
   )
 `).run();
 
@@ -30,24 +42,37 @@ db.prepare(`
   CREATE TABLE IF NOT EXISTS teams (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     country_id INTEGER NOT NULL,
-    year INTEGER NOT NULL,
     event TEXT NOT NULL,
-    round TEXT NOT NULL,
-    result REAL NOT NULL,
+    gender TEXT NOT NULL,
     FOREIGN KEY (country_id) REFERENCES countries(id),
-    UNIQUE(country_id, year, event, round)
+    UNIQUE(country_id, event, gender)
+  )
+`).run();
+
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS team_performance (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    team_id INTEGER NOT NULL,
+    year INTEGER NOT NULL,
+    round TEXT NOT NULL,
+    result REAL,
+    status TEXT,
+    FOREIGN KEY (team_id) REFERENCES teams(id),
+    UNIQUE(team_id, year, round)
   )
 `).run();
 
 db.prepare(`
   CREATE TABLE IF NOT EXISTS team_athletes (
     team_id INTEGER NOT NULL,
+    team_performance_id INTEGER NOT NULL,
     athlete_id INTEGER NOT NULL,
     leg INTEGER NOT NULL,
     FOREIGN KEY (athlete_id) REFERENCES athletes(id),
     FOREIGN KEY (team_id) REFERENCES teams(id),
-    UNIQUE(team_id, athlete_id),
-    UNIQUE(team_id, leg)
+    FOREIGN KEY (team_performance_id) REFERENCES team_performance(id),
+    UNIQUE(team_id, team_performance_id, athlete_id),
+    UNIQUE(team_id, team_performance_id, leg)
   )
 `).run();
 
